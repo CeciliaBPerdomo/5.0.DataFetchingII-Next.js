4e92ef58b6601e6a9f1ce6837b1dc540c483ab45
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-02-01 16:38:16 -0300

    4.2 Caching y revalidate

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -2,7 +2,12 @@ import React from 'react'
 import Link from 'next/link'
 
 const getPosts = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        cache: "force-cache",           // Viene por defecto y fuerza al cache --> no-store: no cache la respuesta    
+                                        // Retorna lo que tiene guardado en el cache
+        next: { revalidate: 3600 }      // hace el refetch cada una hora
+                                        // si se pone en 0, se hace bajo demanda
+    }) 
 
     if (!response.ok) {
         throw new Error("Fallo la obtencion de datos, men!")
@@ -16,18 +21,14 @@ const Posts = async () => {
     const posts = await getPosts()
     return (
         <div className='container m-auto mt-6'>
-            <h1>Posts</h1>
+            <h1 className='text-2xl'>Posts</h1>
             <hr />
             <ul>
                 {posts.map((post) => (
-                    <Link href={`/posts/${post.id}`}>
-                    <li
-                        key={post.id}
-                        className='my-4 ml-8 list-disc'
-                    >
-                        <b>
-                            {post.title}
-                        </b>: {post.body}
+                    <Link href={`/posts/${post.id}`} key={post.id}>
+                    <li  
+                        className='my-4 ml-6'
+                    > {post.id}. <b>{post.title}</b>
                     </li>
                     </Link>
                 ))}
