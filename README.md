ac9bb67e2b3476f744907f8635d962f69fbea201
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-02-05 17:35:45 -0300

    5.3 - Integrando Firebase

--- README.md
+++ README.md
@@ -44,6 +44,9 @@ You can start editing the page by modifying `app/page.js`. The page auto-updates
 
 This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
 
+## Instalación de firebase:
+- npm install firebase
+
 ## Learn More
 
 To learn more about Next.js, take a look at the following resources:
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBidhwrySVcFBPqjl7Wrhg7LpVScDwFfaw",
+  authDomain: "coder-nextjs.firebaseapp.com",
+  projectId: "coder-nextjs",
+  storageBucket: "coder-nextjs.appspot.com",
+  messagingSenderId: "191677628387",
+  appId: "1:191677628387:web:38b139854d084741d1ead6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
